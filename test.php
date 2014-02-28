5fe8f28b4d4c3ee5de34dae4f2af10fbc19e0d3b
ka0ru <ayuu.kaw@gmail.com>
2014-02-28 15:04:06 +0100

    test.php

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+
+Function test_compte_nb_mots_ligne(){
+	return (
+		3 == countWord("Salut les gens")
+	);
+}
+
+Function test_compte_nb_mots_ligne_two(){
+	return (
+		3 == countWord(" Salut les gens ")
+	);
+}
+
+Function test_compte_nb_mots_ligne_three(){
+	return (
+		3 == countWord("Salut  les  gens")
+	);
+}
+
+Function test_compte_nb_mots_ligne_four(){
+	return (
+		countWord("Salut les   gens \n
+			l'extreme programming est cool !") == 7
+	);
+}
+
+Function countWord($string){
+	return str_word_count($string);
+	// return count(explode(" ", trim($string)));
+}
+
+Function lance_tests(){
+	var_dump(test_compte_nb_mots_ligne());
+	var_dump(test_compte_nb_mots_ligne_two());
+	var_dump(test_compte_nb_mots_ligne_three());
+	var_dump(test_compte_nb_mots_ligne_four());
+}
+
+lance_tests();
+
+
+?>
